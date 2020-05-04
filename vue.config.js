5dfd9a930183a28c453aad1d59a16d122e0025c4
yukelin <728820284@qq.com>
2020-05-04 23:43:54 +0800

    .

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,8 @@ module.exports = {
     // }
 
 
-    publicPath: '', //根路径
+    publicPath: '/', //根路径
+    // assetsPublicPath: './',
     outputDir: "dist", //构建输出目录
     assetsDir: "assets", //静态资源目录　（js, css, img, fonts）
     lintOnSave: false, //是否开启estlint保存检查
